3bbf06e9ed9988615706499a57c43604e17ca6d5
julitan <49323514+julitan@users.noreply.github.com>
2023-10-01 11:51:48 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+
+
+
+chat_model = ChatOpenAI()
+
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해주세요')
+
+if st.button('시 요청하기'):
+    with st.spinner('오오 시상이 떠오른다~'):
+        result = chat_model.predict(content + "에 대한 시를 써줘")
+        st.write(result)
+
+
+# print(result)
+
+
+
+
+
+
